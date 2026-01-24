c0e50dd5b2375e2f71563a7458f13abcf7197fa9
Adrian <lucygomu525@gmail.com>
2026-01-24 08:40:08 +0700

    revisi sidebar

--- resources/views/components/sidebars/admin_transport.blade.php
+++ resources/views/components/sidebars/admin_transport.blade.php
@@ -78,7 +78,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center py-4" href="#">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center py-4" href="/admin-transport">
         
         <div class="sidebar-brand-text">
             <div class="brand-title">
