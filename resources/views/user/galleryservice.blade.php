4a684fe51c633dbecb57608cf6b56f82fad97531
Adrian <lucygomu525@gmail.com>
2026-01-24 11:17:51 +0700

    Add all gallery images

--- resources/views/user/galleryservice.blade.php
+++ resources/views/user/galleryservice.blade.php
@@ -284,7 +284,7 @@ document.addEventListener('DOMContentLoaded', () => {
 <script>
 const media = [
     { type: 'video', src: "{{ asset('assets/galleryservice/vid1.mp4') }}" },
-    { type: 'video', src: "{{ asset('assets/galleryservice/WhatsApp Video 2026-01-23 at 17.42.23.mp4') }}" },
+    { type: 'video', src: "{{ asset('assets/galleryservice/vid2.mp4') }}" },
     { type: 'image', src: "{{ asset('assets/galleryservice/service1.jpg') }}" },
     { type: 'image', src: "{{ asset('assets/galleryservice/service2.jpg') }}" },
     { type: 'image', src: "{{ asset('assets/galleryservice/service3.jpg') }}" },
@@ -294,9 +294,10 @@ const media = [
     { type: 'image', src: "{{ asset('assets/galleryservice/service7.jpeg') }}" },
     { type: 'image', src: "{{ asset('assets/galleryservice/service8.jpeg') }}" },
     { type: 'image', src: "{{ asset('assets/galleryservice/service9.jpg') }}" },
-    { type: 'image', src: "{{ asset('assets/galleryservice/WhatsApp Image 2026-01-23 at 17.41.07.jpeg') }}" },
-    { type: 'image', src: "{{ asset('assets/galleryservice/WhatsApp Image 2026-01-23 at 17.43.05.jpeg') }}" },
-    { type: 'image', src: "{{ asset('assets/galleryservice/WhatsApp Image 2026-01-23 at 17.51.27.jpeg') }}" },
+    { type: 'image', src: "{{ asset('assets/galleryservice/service10.jpg') }}" },
+    { type: 'image', src: "{{ asset('assets/galleryservice/service11.jpg') }}" },
+    { type: 'image', src: "{{ asset('assets/galleryservice/service12.jpg') }}" },
+   
     
 ];
 
