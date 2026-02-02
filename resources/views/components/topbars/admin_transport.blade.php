dac04f1259cb747f7bc6c7630401a885b4a921d5
Adrian <lucygomu525@gmail.com>
2026-02-02 10:38:50 +0700

    fix logout modal

--- resources/views/components/topbars/admin_transport.blade.php
+++ resources/views/components/topbars/admin_transport.blade.php
@@ -90,6 +90,40 @@
 
     </ul>
 </nav>
+<!-- Logout Modal -->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog"
+     aria-labelledby="logoutModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h5 class="modal-title" id="logoutModalLabel">Konfirmasi Logout</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                Apakah kamu yakin ingin keluar dari Sistem?
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                    Batal
+                </button>
+
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="btn btn-danger">
+                        Logout
+                    </button>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
+
 
 <!-- Spacer supaya konten nggak ketutup topbar -->
 <div style="height:70px;"></div>
--- resources/views/components/topbars/owner.blade.php
+++ resources/views/components/topbars/owner.blade.php
@@ -104,6 +104,41 @@
 
 <!-- CSRF -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
+<!-- Logout Modal -->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog"
+     aria-labelledby="logoutModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+
+            <div class="modal-header">
+                <h5 class="modal-title" id="logoutModalLabel">Konfirmasi Logout</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+
+            <div class="modal-body">
+                Apakah Anda yakin ingin keluar dari sistem?
+            </div>
+
+            <div class="modal-footer">
+                <button type="button"
+                        class="btn btn-secondary"
+                        data-dismiss="modal">
+                    Batal
+                </button>
+
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="btn btn-danger">
+                        Logout
+                    </button>
+                </form>
+            </div>
+
+        </div>
+    </div>
+</div>
 
 <!-- NOTIF SCRIPT -->
 <script>
